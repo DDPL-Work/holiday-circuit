13edabfd462f24c44c246f71153d76b30df38135
Sudhanshu Gaur <sudhanshu2246@gmail.com>
2026-09-12 12:37:58 +0530

    change the cors links

--- server/index.js
+++ server/index.js
@@ -19,22 +19,22 @@ const REQUEST_BODY_LIMIT = process.env.REQUEST_BODY_LIMIT || "25mb";
 dbConnect();
 
 // ====================== MIDDLEWARE ==================
-// const allowedOrigins = [
-//   "https://holidaycircuit.com",
-//   "https://www.holidaycircuit.com",
-//   "https://admin.holidaycircuit.com",
-//   "https://agent.holidaycircuit.com",
-//   "https://dmc.holidaycircuit.com",
-//   "https://ops.holidaycircuit.com",
-//   "https://finance.holidaycircuit.com",
-// ];
 const allowedOrigins = [
-  "http://localhost:5173",
-  "http://localhost:5174",
-  "http://localhost:5175",
-  "http://localhost:5176",
-  "http://localhost:5177",
+  "https://holidaycircuit.com",
+  "https://www.holidaycircuit.com",
+  "https://admin.holidaycircuit.com",
+  "https://agent.holidaycircuit.com",
+  "https://dmc.holidaycircuit.com",
+  "https://ops.holidaycircuit.com",
+  "https://finance.holidaycircuit.com",
 ];
+// const allowedOrigins = [
+//   "http://localhost:5173",
+//   "http://localhost:5174",
+//   "http://localhost:5175",
+//   "http://localhost:5176",
+//   "http://localhost:5177",
+// ];
 
 app.use(cors({
   origin: allowedOrigins,
